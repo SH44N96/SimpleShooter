f30d478f4053c6a44ae1fffa7737d8181f37c27b
SH44N96 <sh44n96@gmail.com>
2024-07-06 13:07:45 +0800

    Spawning Actors at Runtime

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class AGun;
+
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -15,15 +17,20 @@ public:
 	// Sets default values for this character's properties
 	AShooterCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass;
+
+	UPROPERTY()
+	AGun* Gun;
 };
