e6dc57fc623902324967171965660c641108ac60
SH44N96 <sh44n96@gmail.com>
2024-07-07 12:14:24 +0800

    NavMesh and AI Movement

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -10,3 +10,11 @@ void AShooterAIController::BeginPlay()
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 	SetFocus(PlayerPawn);
 }
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	MoveToActor(PlayerPawn, 200);
+}
